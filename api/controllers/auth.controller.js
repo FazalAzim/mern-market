63afcc89d9426cf02a27f3450f6cc1e352bdab78
FazalAzim <fazalazim238@gmail.com>
2024-01-03 12:57:20 +0500

    create middleware and function for error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,11 +1,11 @@
 import User from "../models/user.model.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
 	try {
 		const newUser = new User(req.body);
 		await newUser.save();
 		res.status(201).json("user created successfully");
 	} catch (error) {
-		res.status(400).json(error.message);
+		next(error);
 	}
 };
--- api/index.js
+++ api/index.js
@@ -17,3 +17,13 @@ app.listen(port, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+app.use((error, req, res, next) => {
+	const statusCode = error.statusCode || 500;
+	const message = error.message || "Internal Server Error";
+	return res.status(statusCode).json({
+		success: false,
+		statusCode,
+		message,
+	});
+});
