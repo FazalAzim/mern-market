bb09b4ab0a6acae22fd2854bd5ae5e3c1dffc1bc
FazalAzim <fazalazim238@gmail.com>
2024-01-04 15:13:27 +0500

    add redux persist localstorage

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -7,8 +7,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { privateRoutes, publicRoutes } from "./config/routes.jsx";
 import { ErrorPage } from "./pages/errorPage.jsx";
-import { store } from "./redux/store.js";
+import { persistor, store } from "./redux/store.js";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 const App = () => {
@@ -35,7 +36,9 @@ const App = () => {
 root.render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<App />
+			<PersistGate loading={null} persistor={persistor}>
+				<App />
+			</PersistGate>
 		</Provider>
 	</React.StrictMode>
 );
